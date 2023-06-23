20e7cd1502dca4005af09e3918513a4bc93bceb6
FedericoOlive <federicoolive@gmail.com>
2023-06-23 13:10:42 -0300

    Clase 230623

--- _Temporal/Main.cpp
+++ _Temporal/Main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-#include <ctime>
-
-using namespace std;
 
 void main()
 {
-	
+	// Deteccion de a-z vs 0-9
+	char input = 'a';
+
+	while (true)
+	{
+		std::cout << "\n\n";
+		std::cin >> input;
+		bool isAlpha = isalpha(input);
+		bool isDigit = isdigit(input);
+		if (isAlpha)
+			std::cout << "Es alpha\n";
+		else
+			std::cout << "NO Es alpha\n";
+		if (isDigit)
+			std::cout << "Es digit\n";
+		else
+			std::cout << "NO digit\n";
+	}
+
+
+	// Clear de bufer de teclado (cin)
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
